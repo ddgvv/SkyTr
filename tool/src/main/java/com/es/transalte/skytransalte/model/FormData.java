7fbda04d84d814544796e5b50dcb60f4fe64bcb9
VigneswarV <165656044+VigneswarV@users.noreply.github.com>
2024-11-13 20:36:04 +0530

    Feat - Project Initialize
    First Page Implementation

---
+++ tool/src/main/java/com/es/transalte/skytransalte/model/FormData.java
@@ -0,0 +1,13 @@
+package com.es.transalte.skytransalte.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FormData {
+    private String dealerName;
+    private String customerName;
+    private String accountType;
+    private String term;
+}
---
+++ tool/src/test/java/com/es/transalte/skytransalte/ToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es.transalte.skytransalte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
