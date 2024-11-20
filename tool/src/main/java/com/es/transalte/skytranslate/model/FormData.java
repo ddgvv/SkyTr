e8ab660a71ff5bafd093484a5493bdb45ee599f8
VigneswarV <165656044+VigneswarV@users.noreply.github.com>
2024-11-20 23:26:21 +0530

    Lombok remove

--- tool/src/main/java/com/es/transalte/skytranslate/model/FormData.java
+++ tool/src/main/java/com/es/transalte/skytranslate/model/FormData.java
@@ -1,13 +1,40 @@
 package com.es.transalte.skytranslate.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class FormData {
     private String dealerName;
     private String customerName;
     private String accountType;
     private String term;
+
+    public String getDealerName() {
+        return dealerName;
+    }
+
+    public void setDealerName(String dealerName) {
+        this.dealerName = dealerName;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
 }
