396b72cf035523cbbea1411609bd9dd022699ed3
ravikantgour <ravikantgour36@gmail.com>
2024-11-21 22:13:54 +0530

    homepage
    worked on home page

--- tool/src/main/java/com/es/transalte/skytranslate/config/WebConfig.java
+++ tool/src/main/java/com/es/transalte/skytranslate/config/WebConfig.java
@@ -12,5 +12,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("classpath:/static/images/");
+        // For CSS files
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/");
+        // For font files
+        registry.addResourceHandler("/fonts/**")
+                .addResourceLocations("classpath:/static/fonts/");
     }
 }
